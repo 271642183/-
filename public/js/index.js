581e196a733517cf0937046a64e762954335c1f9
271642183 <271642183@qq.com>
2017-11-25 11:04:18 +0800

    about,banner

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 	var banSwiper = new Swiper('.ban-swiper-container',{
+		autoplay: 3000,
 		pagination : '.ban-pagination',
+
+	});
+	var culturalSwiper = new Swiper('.cultural-swiper-container',{
+		autoplay: 3000,
+    	nextButton: '.cultural-swiper-next',
+   		prevButton:'.cultural-swiper-prev',
 
 	});
 	$('.nav-btn').mousemove(function(){
